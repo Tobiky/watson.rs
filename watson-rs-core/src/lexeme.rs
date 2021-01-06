30be71174a96c1a171de3d79cde066b2f0fa9cf2
Tobiky <andreas.hammarstrand@gmail.com>
2021-01-06 17:38:17 +0100

    tests: unit tests for lexeme module

--- watson-rs-core/src/lexeme.rs
+++ watson-rs-core/src/lexeme.rs
@@ -44,3 +44,144 @@ pub fn all_bindings() -> Vec<(Mode, Vec<(LexemeType, Instruction)>)> {
         .map(|mode| (mode, bindings(mode)))
         .collect::<Vec<(Mode, Vec<(LexemeType, Instruction)>)>>()
 }
+
+#[cfg(test)]
+mod unit_tests {
+    use crate::mode::Mode;
+    use super::*;
+
+    #[test]
+    fn test_mode_lexeme_sequences() {
+        assert_eq!(MODE_A_LEXEMES_SEQUENCES.iter().collect::<String>(), String::from("BubaAei'qtp?!~M@szo.E#%"));
+        assert_eq!(MODE_S_LEXEMES_SEQUENCES.iter().collect::<String>(), String::from("ShakrAzimbu$-+gv?^!y/e:"));
+    }
+
+    #[test]
+    fn test_lexeme_sequences() {
+        assert_eq!(lexeme_sequences(Mode::A), MODE_A_LEXEMES_SEQUENCES.iter().cloned().collect::<Vec<_>>());
+        assert_eq!(lexeme_sequences(Mode::S), MODE_S_LEXEMES_SEQUENCES.iter().cloned().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn test_mode_a_bindings() {
+        let mode_bindings = vec![
+            ('B', Instruction::Inew), 
+            ('u', Instruction::Iinc), 
+            ('b', Instruction::Ishl), 
+            ('a', Instruction::Iadd), 
+            ('A', Instruction::Ineg), 
+            ('e', Instruction::Isht), 
+            ('i', Instruction::Itof),
+            ('\'', Instruction::Itou),
+            ('q', Instruction::Finf), 
+            ('t', Instruction::Fnan), 
+            ('p', Instruction::Fneg), 
+            ('?', Instruction::Snew), 
+            ('!', Instruction::Sadd), 
+            ('~', Instruction::Onew), 
+            ('M', Instruction::Oadd), 
+            ('@', Instruction::Anew), 
+            ('s', Instruction::Aadd), 
+            ('z', Instruction::Bnew), 
+            ('o', Instruction::Bneg),
+            ('.', Instruction::Nnew), 
+            ('E', Instruction::Gdup), 
+            ('#', Instruction::Gpop), 
+            ('%', Instruction::Gswp),
+        ];
+
+        assert_eq!(bindings(Mode::A), mode_bindings);
+    }
+    #[test]
+    fn test_mode_s_bindings() {
+        let mode_bindings = vec![
+            ('S', Instruction::Inew), 
+            ('h', Instruction::Iinc), 
+            ('a', Instruction::Ishl), 
+            ('k', Instruction::Iadd), 
+            ('r', Instruction::Ineg), 
+            ('A', Instruction::Isht), 
+            ('z', Instruction::Itof),
+            ('i', Instruction::Itou),
+            ('m', Instruction::Finf), 
+            ('b', Instruction::Fnan), 
+            ('u', Instruction::Fneg), 
+            ('$', Instruction::Snew), 
+            ('-', Instruction::Sadd), 
+            ('+', Instruction::Onew), 
+            ('g', Instruction::Oadd), 
+            ('v', Instruction::Anew), 
+            ('?', Instruction::Aadd), 
+            ('^', Instruction::Bnew), 
+            ('!', Instruction::Bneg),
+            ('y', Instruction::Nnew), 
+            ('/', Instruction::Gdup), 
+            ('e', Instruction::Gpop), 
+            (':', Instruction::Gswp),
+        ];
+
+        assert_eq!(bindings(Mode::S), mode_bindings);
+    }
+
+    #[test]
+    fn test_all_bindings() {
+        let mode_a_bindings = vec![
+            ('B', Instruction::Inew), 
+            ('u', Instruction::Iinc), 
+            ('b', Instruction::Ishl), 
+            ('a', Instruction::Iadd), 
+            ('A', Instruction::Ineg), 
+            ('e', Instruction::Isht), 
+            ('i', Instruction::Itof),
+            ('\'', Instruction::Itou),
+            ('q', Instruction::Finf), 
+            ('t', Instruction::Fnan), 
+            ('p', Instruction::Fneg), 
+            ('?', Instruction::Snew), 
+            ('!', Instruction::Sadd), 
+            ('~', Instruction::Onew), 
+            ('M', Instruction::Oadd), 
+            ('@', Instruction::Anew), 
+            ('s', Instruction::Aadd), 
+            ('z', Instruction::Bnew), 
+            ('o', Instruction::Bneg),
+            ('.', Instruction::Nnew), 
+            ('E', Instruction::Gdup), 
+            ('#', Instruction::Gpop), 
+            ('%', Instruction::Gswp),
+        ];
+
+        let mode_s_bindings = vec![
+            ('S', Instruction::Inew), 
+            ('h', Instruction::Iinc), 
+            ('a', Instruction::Ishl), 
+            ('k', Instruction::Iadd), 
+            ('r', Instruction::Ineg), 
+            ('A', Instruction::Isht), 
+            ('z', Instruction::Itof),
+            ('i', Instruction::Itou),
+            ('m', Instruction::Finf), 
+            ('b', Instruction::Fnan), 
+            ('u', Instruction::Fneg), 
+            ('$', Instruction::Snew), 
+            ('-', Instruction::Sadd), 
+            ('+', Instruction::Onew), 
+            ('g', Instruction::Oadd), 
+            ('v', Instruction::Anew), 
+            ('?', Instruction::Aadd), 
+            ('^', Instruction::Bnew), 
+            ('!', Instruction::Bneg),
+            ('y', Instruction::Nnew), 
+            ('/', Instruction::Gdup), 
+            ('e', Instruction::Gpop), 
+            (':', Instruction::Gswp),
+        ];
+
+        let all_mode_bindings = vec![ 
+            (Mode::A, mode_a_bindings), 
+            (Mode::S, mode_s_bindings) 
+        ];
+
+        assert_eq!(all_mode_bindings, all_bindings());
+    }
+}
