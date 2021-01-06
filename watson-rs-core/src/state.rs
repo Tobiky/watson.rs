39706ba9bd8b029ceafecc1bdf9f3e2adc9487ca
Tobiky <andreas.hammarstrand@gmail.com>
2021-01-06 18:18:56 +0100

    tests: unit tests for state

--- watson-rs-core/src/state.rs
+++ watson-rs-core/src/state.rs
@@ -20,10 +20,6 @@ impl State {
         }
     }
 
-    pub fn next_mode(&mut self) {
-        self.mode = self.mode.next_mode();
-    }
-
     pub fn mode(&self) -> Mode {
         self.mode
     }
@@ -34,6 +30,10 @@ impl State {
 
     pub fn column(&self) -> usize {
         self.column
+    }
+
+    pub fn next_mode(&mut self) {
+        self.mode = self.mode.next_mode();
     }
 
     pub fn increment_line(&mut self) {
@@ -50,5 +50,63 @@ impl State {
 
     pub fn reset_column(&mut self) {
         self.column = 0;
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use crate::mode::Mode;
+
+    use super::State;
+
+    #[test]
+    fn create_state() {
+        let state = State::new();
+        assert_eq!(state.mode, Mode::A);
+        assert_eq!(state.column, 0);
+        assert_eq!(state.line, 0);
+
+        let state = State::with_mode(Mode::S);
+        assert_eq!(state.mode, Mode::S);
+        assert_eq!(state.column, 0);
+        assert_eq!(state.line, 0);
+    }
+
+    #[test]
+    fn switch_modes() {
+        let mut state = State::new();
+        assert_eq!(state.mode, Mode::A);
+        state.next_mode();
+        assert_eq!(state.mode, Mode::S);
+        state.next_mode();
+        assert_eq!(state.mode, Mode::A);
+    }
+
+    #[test]
+    fn increment() {
+        let mut state = State::new();
+        assert_eq!(state.column, 0);
+        assert_eq!(state.line,   0);
+
+        state.increment_column();
+        assert_eq!(state.column, 1);
+        assert_eq!(state.line,   0);
+        
+        state.increment_line();
+        assert_eq!(state.column, 1);
+        assert_eq!(state.line,   1);
+    }
+
+    #[test]
+    fn reset() {
+        let mut state = State::new();
+        state.increment_column();
+        state.increment_line();
+
+        state.reset_column();
+        assert_eq!(state.column, 0);
+
+        state.reset_line();
+        assert_eq!(state.line, 0);
     }
 }
